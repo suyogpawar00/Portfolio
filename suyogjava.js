9b5e5a85ad744d71383fc69bfd38b5af7407dfea
suyogpawar00 <104990500+suyogpawar00@users.noreply.github.com>
2026-02-07 15:59:04 -0600

    Add files via upload

---
+++ suyogjava.js
@@ -0,0 +1,68 @@
+//Toggling Menu
+const showMenu = (toggleId, navId) => {
+    const toggle = document.getElementById(toggleId);
+    const nav = document.getElementById(navId);
+
+    if(toggle && nav) {
+        toggle.addEventListener('click', () => {
+            nav.classList.toggle('show');
+        })
+    }
+}
+
+showMenu('nav-toggle', 'nav-menu');
+
+//Toggling Active Link
+const navLink = document.querySelectorAll('.nav-link');
+
+function linkAction() {
+    navLink.forEach(n => n.classList.remove('active'));
+    this.classList.add('active');
+
+    const navMenu = document.getElementById('nav-menu');
+    navMenu.classList.remove('show');
+}
+
+navLink.forEach(n => n.addEventListener('click', linkAction));
+
+// Scroll Reveal
+
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '80px',
+    duration: 2000,
+    // reset: true
+})
+
+sr.reveal('.home-title', {} )
+sr.reveal('.button', {delay: 200} )
+sr.reveal('.home-img', {delay: 400} )
+sr.reveal('.home-social', {delay: 400,} )
+
+sr.reveal('.about-img', {} )
+sr.reveal('.about-subtitle', {delay: 200} )
+sr.reveal('.about-text', {delay: 400} )
+
+sr.reveal('.skills-subtitle', {delay: 100} )
+sr.reveal('.skills-text', {delay: 100} )
+sr.reveal('.skills-data', {interval: 100} )
+sr.reveal('.skills-img', {delay: 400} )
+
+// sr.reveal('.work-img', {interval: 200} )
+// sr.reveal('.work-card', { interval: 200 });
+
+// Experience section animation
+sr.reveal('.experience-item', { interval: 200 });
+
+
+sr.reveal('.project-item', {interval: 200});
+
+sr.reveal('.contact-input', {interval: 200} )
+sr.reveal('.education-item', {interval: 200});
+
+sr.reveal('.achievement-item', { interval: 200 });
+sr.reveal('.achievements-list li', { interval: 200 });
+
+sr.reveal('.certification-item', { interval: 200 });
+
+sr.reveal('.leadership-item', { interval: 200 });
